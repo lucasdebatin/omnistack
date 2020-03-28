c8233118b1cd1eba0ba5c202d54cf95a9e88d4b3
Lucas Debatin <lucasdebatinb@gmail.com>
2020-03-28 14:56:26 -0300

    commit 1 - backend

---
+++ backend/src/routes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const routes = express.Router();
+const connection = require('./database/connection');
+const ongController = require('./controllers/ongController');
+const incidentController = require('./controllers/incidentController');
+const profileController = require('./controllers/profileController');
+const sessionController = require('./controllers/sessionController');
+
+routes.get('/', (request, response) => {
+    return response.send('vaitefude');
+});
+
+routes.post('/sessions', sessionController.create);
+
+routes.get('/ongs', ongController.index);
+routes.post('/ongs', ongController.create);
+
+routes.get('/incidents', incidentController.index);
+routes.post('/incidents', incidentController.create);
+routes.delete('/incidents/:id', incidentController.delete);
+
+routes.get('/profile', profileController.index);
+
+module.exports = routes;
